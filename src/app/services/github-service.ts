a98c01f316fde5b9ce62b5ba6f72fd37135f9995
aydenxiao <66319244+bcx20@users.noreply.github.com>
2023-10-31 22:36:35 -0400

    updated tokens

--- src/app/services/github-service.ts
+++ src/app/services/github-service.ts
@@ -12,9 +12,9 @@ export class GithubService {
   private map: Map<string, IRepo> = new Map<string, IRepo>();
   private octokit: Octokit;
   private fetchTokenResponse: any = {
-    "access_token": "ghu_9tDTNtnG0zdhBQbgE1DQMIlHLlNfZf2UxTVg",
+    "access_token": "ghu_DPLvBlbPfU0k5P9TdYi70n6NpVru2944Hbrg",
     "expires_in": 28800,
-    "refresh_token": "ghr_uYAyqAvqfyRysI9JTOcLWsLvYTYT3G6FOIONXJFtP9MReLVNHaS6NQ3c1yRw9yejcOO6PJ0nTX6A",
+    "refresh_token": "ghr_XtUp84ETuySAWQ0TebksRiTcNqCOue24X5v9vMZObsuoVcnrK2PQdPNGgqVn1tweit1k4s3cAp7U",
     "refresh_token_expires_in": 15724800,
     "token_type": "bearer",
     "scope": ""
